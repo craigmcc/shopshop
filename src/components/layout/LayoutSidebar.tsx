3cfbf4596754addf839b961110ee75469aab78e6
Craig McClanahan <craigmcc@gmail.com>
2023-10-29 11:52:31 -0700

    Basic version of the overall layout.

---
+++ src/components/layout/LayoutSidebar.tsx
@@ -0,0 +1,25 @@
+// @/components/layout/LayoutHeader.tsx
+
+/**
+ * Side-of-layout menu bar for the global layout.
+ *
+ * @packageDocumentation
+ */
+
+// External Modules ----------------------------------------------------------
+
+// Internal Modules ----------------------------------------------------------
+
+// Public Objects ------------------------------------------------------------
+
+export const LayoutSidebar = () => {
+
+    return (
+        <aside
+            className="hidden md:flex h-full w-[72px] flex-col fixed bg-indigo-200 dark:bg-indigo-800"
+        >
+            <p>Layout Sidebar with some extra height</p>
+        </aside>
+    )
+
+}
