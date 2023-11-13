0873765c88d23bd3324261b7125f27a153756f19
Craig McClanahan <craigmcc@gmail.com>
2023-11-12 18:05:06 -0800

    Make the initials in ListSelectItem extra small so they fit better.

--- src/components/lists/ListSelectItem.tsx
+++ src/components/lists/ListSelectItem.tsx
@@ -63,7 +63,7 @@ export const ListSelectItem = ({
                             src={list.imageUrl}
                         />
                     ) : (
-                        <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+                        <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 text-xs">
                             {initials(list.name)}
                         </div>
                     )}
