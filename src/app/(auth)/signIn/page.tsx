e395d02f209065c077d4d9bcb5f27eaa5fc7da63
Craig McClanahan <craigmcc@gmail.com>
2024-12-14 15:22:02 -0800

    Shells of signIn and signUp pages.

---
+++ src/app/(auth)/signIn/page.tsx
@@ -0,0 +1,19 @@
+// @/app/(auth)/signUp/page.tsx
+
+/**
+ * Sign up to authenticate an existing profile.
+ *
+ * @packageDocumentation
+ */
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1>TODO - Sign In page</h1>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/app/(auth)/signUp/page.tsx
@@ -0,0 +1,19 @@
+// @/app/(auth)/signUp/page.tsx
+
+/**
+ * Sign up to create a new profile page.
+ *
+ * @packageDocumentation
+ */
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1>TODO - Sign Up page</h1>
+        </div>
+      </main>
+    </div>
+  )
+}
