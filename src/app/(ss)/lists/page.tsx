6b6c3af20bbf2092f6daf82ee69cc204116bceea
Craig McClanahan <craigmcc@gmail.com>
2024-12-14 19:14:31 -0800

    Dummy lists page to be navigated to on successful sign in.

---
+++ src/app/(ss)/lists/page.tsx
@@ -0,0 +1,20 @@
+// @/app/(auth)/lists/page.tsx
+
+/**
+ * Enumerate the lists that this profile is a member of.
+ * Successful sign ins will be redirected to this page.
+ *
+ * @packageDocumentation
+ */
+
+export default function ListsPage() {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1>TODO - Lists page</h1>
+        </div>
+      </main>
+    </div>
+  )
+}
