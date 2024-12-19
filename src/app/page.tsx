644255752304e6f7e08f0b8c65faa8f7b5695cc5
Craig McClanahan <craigmcc@gmail.com>
2024-12-19 14:12:26 -0800

    Tweak home page to center content with larger text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <div>
       <main>
-        <div>
-          <h1>TODO - Home Page</h1>
+        <div className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
+          <h1 className="text-5xl">TODO - Home Page</h1>
         </div>
       </main>
     </div>
