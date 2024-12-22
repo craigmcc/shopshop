f72efaf0eb97edd4ba2942321709272ef8c400ff
Craig McClanahan <craigmcc@gmail.com>
2024-12-22 12:39:13 -0800

    Install sentry, and update next and next-safe-action.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d38fd71d8941f3ee81bacf720394ff48@o4508338880577536.ingest.us.sentry.io/4508513649754112",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
