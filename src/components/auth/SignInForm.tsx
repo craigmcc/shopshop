069b31422d372d9054cb72d1e4cd4761d7b36d1a
Craig McClanahan <craigmcc@gmail.com>
2024-12-26 19:29:50 -0800

    Use card view on SignInForm. TODO: Sign out works but does not redirect and does not log on the form.

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -21,6 +21,14 @@ import { doSignIn } from "@/actions/authActions";
 import { InputWithLabel } from "@/components/inputs/InputWithLabel";
 import MessageBox, { MessageBoxProps } from "@/components/shared/MessageBox";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+//  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Form } from "@/components/ui/form";
 import { logger } from "@/lib/ClientLogger"
 import { signInSchema, type signInSchemaType } from "@/zod-schemas/signInSchema";
@@ -70,83 +78,70 @@ export default function SignInForm() {
     }
   }
 
-/*
-  async function submitForm(formData: signInSchemaType) {
-    try {
-      const response = await doSignIn(formData);
-      logger.info({
-        context: "SignInForm.submitForm.output",
-        response: response,
-      });
-      redirect("/"); // TODO - probably go to lists page when it exists
-    } catch (error) {
-      logger.info({
-        context: "SignInForm.submitForm.error",
-        error: error,
-      })
-      alert("Incorrect credentials");
-    }
-
-  }
-*/
-
   return (
-    <div className={"flex flex-col gap-1 sm:px-8"}>
-      { result && (
-        <MessageBox content={result.content} type={result.type}/>
-      )}
-      <div>
-        <h2 className="text-2xl font-bold">
-          Sign In To ShopShop
-        </h2>
-      </div>
-      <Form {...form}>
-        <form
-          className="flex flex-col md:flex-row gap-4 md:gap-8"
-          onSubmit={form.handleSubmit(performSignIn)}
-        >
-          <div className="flex flex-col gap-4 w-full max-w-xs">
+    <Card className={"items-center justify-center"}>
+      <CardHeader>
+        <CardTitle>Sign In to ShopShop</CardTitle>
+        <CardDescription>
+          { result && (
+            <MessageBox content={result.content} type={result.type}/>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className={"flex flex-col gap-1 sm:px-8"}>
 
-            <InputWithLabel<signInSchemaType>
-              autoFocus
-              fieldTitle="Email Address:"
-              nameInSchema="email"
-            />
+          <Form {...form}>
+            <form
+              className="flex flex-col md:flex-row gap-4 md:gap-8"
+              onSubmit={form.handleSubmit(performSignIn)}
+            >
+              <div className="flex flex-col gap-4 w-full max-w-xs">
 
-            <InputWithLabel<signInSchemaType>
-              fieldTitle="Password:"
-              nameInSchema="password"
-              type="password"
-            />
+                <InputWithLabel<signInSchemaType>
+                  autoFocus
+                  fieldTitle="Email Address:"
+                  nameInSchema="email"
+                />
 
-            <div className="flex gap-2">
+                <InputWithLabel<signInSchemaType>
+                  fieldTitle="Password:"
+                  nameInSchema="password"
+                  type="password"
+                />
 
-              <Button
-                className="w-3/4"
-                // disabled={isSaving}
-                title="Sign In"
-                type="submit"
-                variant="default"
-              >
-                  Sign In
-              </Button>
+                <div className="flex gap-2">
 
-              <Button
-                onClick={() => {
-                  form.reset(defaultValues)
-                }}
-                title="Reset"
-                type="button"
-                variant="destructive"
-              >
-                Reset
-              </Button>
+                  <Button
+                    className="w-3/4"
+                    // disabled={isSaving}
+                    title="Sign In"
+                    type="submit"
+                    variant="default"
+                  >
+                    Sign In
+                  </Button>
 
-            </div>
-          </div>
-        </form>
-      </Form>
-    </div>
+                  <Button
+                    onClick={() => {
+                      form.reset(defaultValues)
+                    }}
+                    title="Reset"
+                    type="button"
+                    variant="destructive"
+                  >
+                    Reset
+                  </Button>
+
+                </div>
+              </div>
+            </form>
+          </Form>
+
+        </div>
+      </CardContent>
+
+    </Card>
 
   )
 
