d3548a43ad0d0a7047aedd9427775210806a10aa
Craig McClanahan <craigmcc@gmail.com>
2025-02-14 13:45:05 -0800

    Flesh out tests for SignInForm. TODO: SignInForm only does a toast on credential mismatches.

--- src/components/auth/SignInForm.test.tsx
+++ src/components/auth/SignInForm.test.tsx
@@ -5,23 +5,34 @@
 // External Modules ----------------------------------------------------------
 
 import { render, screen } from "@testing-library/react";
-import { act } from "react";
-import { beforeAll, describe, expect, it, vi } from "vitest";
+import { userEvent } from "@testing-library/user-event";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 // Internal Modules ----------------------------------------------------------
 
-import { SignInForm } from "./SignInForm";
+import { SignInForm } from "@/components/auth/SignInForm";
+import * as SeedData from "@/test/SeedData";
 
 // Test Hooks ----------------------------------------------------------------
 
-beforeAll(() => {
 /*
+beforeAll(() => {
+/!*
   vi.mock("next/navigation", () => ({
     useRouter: () => ({
       push: vi.fn(),
     }),
   }));
+*!/
+  vi.mock("next/navigation");
+});
 */
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+beforeEach(async () => {
   vi.mock("next/navigation");
 });
 
@@ -50,30 +61,77 @@ function elements ()  {
 
 describe("SignInForm", () => {
 
-  it("Renders the form", () => {
+  it("should render the form as expected", () => {
 
     render(<SignInForm/>);
     elements();
 
   });
 
-  it("Renders the form with errors", () => {
+  it("should render with validation errors on empty fields", async () => {
 
+    const user = userEvent.setup();
     render(<SignInForm/>);
-    const { /*emailField, passwordField,*/ submitButton} = elements();
+    const { submitButton } = elements();
 
-    // Submit the form
-    act(() => submitButton.click());
-//    submitButton.click();
-    expect(submitButton).toBeDefined();
-    screen.debug();
+    // Submit the form with no field values
+    await user.click(submitButton);
+//    screen.debug();
 
-    // Check for errors
-//    expect(form).toHaveFormErrors();
-//    expect(emailField).to.have.error.message("Email is required");
-//    expect(passwordField).toHaveErrorMessage("Password is required");
-//    expect(emailField).not.toBeValid();
-//    expect(passwordField).not.toBeValid();
+    // Check for validation errors
+    expect(screen.getByText("Email is required"));
+    expect(screen.getByText("Password is required"));
+
+  });
+
+  it("should successfully sign in with valid data", async () => {
+
+    const user = userEvent.setup();
+    render(<SignInForm/>);
+    const {emailField, passwordField, submitButton} = elements();
+
+    // Populate and submit the form
+    await user.type(emailField, SeedData.PROFILE_EMAIL_FIRST);
+    await user.type(passwordField, "firstpassword"); // Cannot use hashed version
+    await user.click(submitButton);
+
+    // Check for no validation errors
+    expect(screen.queryByText("Email is required")).toBeNull();
+    expect(screen.queryByText("Password is required")).toBeNull();
+    // TODO - SignInForm just puts up a toast
+
+  });
+
+  it("should unsuccessfully sign in with invalid data", async () => {
+
+    const user = userEvent.setup();
+    render(<SignInForm/>);
+    const {emailField, passwordField, submitButton} = elements();
+
+    // Populate and submit the form
+    await user.type(emailField, SeedData.PROFILE_EMAIL_SECOND);
+    await user.type(passwordField, "badpassword");
+    await user.click(submitButton);
+
+    // TODO - SignInForm just puts up a toast
+//    screen.debug();
+
+  });
+
+  it("should unsuccessfully sign in with invalid email", async () => {
+
+    const user = userEvent.setup();
+    render(<SignInForm/>);
+    const {emailField, passwordField, submitButton} = elements();
+
+    // Populate and submit the form
+    await user.type(emailField, "bademail");
+    await user.type(passwordField, "badpassword");
+    await user.click(submitButton);
+
+    // Check for validation errors
+    expect(screen.getByText("Invalid email address"));
+    expect(screen.queryByText("Password is required")).toBeNull();
 
   });
 
